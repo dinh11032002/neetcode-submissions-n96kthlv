eae4ae7190483e2eeea6308881ae293f934045c6
Trương Đình <132062054+dinh11032002@users.noreply.github.com>
2026-04-24 08:55:34 +0700

    Add: kth-distinct-string-in-an-array - submission-0

---
+++ Data Structures & Algorithms/kth-distinct-string-in-an-array/submission-0.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun kthDistinct(arr: Array<String>, k: Int): String {
+        val map = mutableMapOf<String, Int>()
+        for (s in arr) {
+            map[s] = map.getOrDefault(s, 0) + 1
+        }
+
+        var count = 0
+        for (s in arr) {
+            if (map[s] == 1) {
+                count++
+                if (count == k) {
+                    return s
+                }
+            }
+        }
+        return ""
+    }
+}
