7dad09b342f234bb2e6d593959d14551f889bbfc
Trương Đình <132062054+dinh11032002@users.noreply.github.com>
2026-07-21 09:41:00 +0700

    Add: crawler-log-folder - submission-0

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-0.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun minOperations(logs: Array<String>): Int {
+        var depth = 0
+
+        for (log in logs) {
+            when (log) {
+                "../" -> if (depth > 0) depth--
+                "./" -> {  }
+                else -> depth++
+            }
+        }
+
+        return depth
+    }
+}
